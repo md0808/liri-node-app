bd917fcb7a9bb8411f5778cf2def3f76465b9fe3
Monica <monica.dixon8@gmail.com>
2019-11-08 12:47:13 -0600

    Finished movie-this and do-what-it-says functions

--- liri.js
+++ liri.js
@@ -17,6 +17,8 @@ var spotify = new Spotify({
     secret: spotifyKeys.secret
 })
 
+
+//takes in user request 
 var nodeArg = process.argv
 
 var action = process.argv[2];
@@ -31,72 +33,73 @@ for (var i = 3; i < nodeArg.length; i++) {
 }
 
 
-switch (action) {
-    case "concert-this":
-        concertThis();
-        break;
+//evaluates user request
+function evaluateAction(){
+    switch (action) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this-song":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            console.log("Liri doesn't understand you.");
+    }
+}
+evaluateAction();
 
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
-
-    case "movie-this":
-        movieThis();
-        break;
-
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
-    default:
-        console.log("Liri doesn't understand you.");
+function log(info) {
+    fs.appendFile("log.txt", info, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(info);
+    })
 }
 
 //concert-this
 function concertThis() {
     axios.get("https://rest.bandsintown.com/artists/" + mediaSearch + "/events?app_id=codingbootcamp").then(
         function (response) {
-            console.log("\r\n\r\n\r\n*************************");
-            console.log("See " + mediaSearch.replace(/[^\w\s]/gi, " ") + " at " +
+            let info = "\r\n\r\n\r\n*************************"
+                + "\nSee " + mediaSearch.replace(/[^\w\s]/gi, " ") + " at " +
                 "\n" + JSON.stringify(response.data[0].venue.name).replace(/"/g, "") +
                 "\nin " + JSON.stringify(response.data[0].venue.city).replace(/"/g, "") +
                 ", " + JSON.stringify(response.data[0].venue.region).replace(/"/g, "") +
                 ", " + JSON.stringify(response.data[0].venue.country).replace(/"/g, "") +
                 "\nOn " + moment(JSON.stringify(response.data[0].datetime).replace(/"/g, "")).format("MM/DD/YYYY")
-                + "\n*************************\r\n\r\n\r\n ");
+                + "\n*************************\r\n\r\n\r\n ";
+            log(info);
         })
-
         .catch(function (error) {
             if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
+                console.log(error);
             } else {
-                // Something happened in setting up the request that triggered an Error
                 console.log("This band or artist might not be touring right now. Try restating, or someone else. \n*************************\r\n\r\n\r\n ");
             }
 
         });
 }
-
-//mediaSearch.replace(/[^\w\s]/gi, " ")
-//spotify-this-song
-// If no long, it should default to Ace of Base the Sign
 function spotifyThisSong() {
-    spotify.search({ type: "track", query: mediaSearch.replace(/[^\w\s]/gi, " ")})
+    var song = mediaSearch;
+    if (!mediaSearch || mediaSearch == undefined) {
+        song = "The Sign Ace of Base"
+    }
+    spotify.search({ type: "track", query: song.replace(/[^\w\s]/gi, " ") })
         .then(function (response) {
-            console.log(response.tracks.items[0]);
-            console.log("\n\r\n\r\n*************************");
-            console.log("Song title: " +response.tracks.items[0].name);
-            console.log( "Artist Name: "+response.tracks.items[0].artists[0].name);
-            console.log("Album name: " + response.tracks.items[0].album.name);
-            console.log("Link to song: " + response.tracks.items[0].external_urls.spotify);
-            console.log("*************************\n\r\n\r\n");
+            let info = "\n\r\n\r\n*************************" +
+                "\nSong title: " + response.tracks.items[0].name +
+                "\nArtist Name: " + response.tracks.items[0].artists[0].name +
+                "\nAlbum name: " + response.tracks.items[0].album.name +
+                "\nLink to song: " + response.tracks.items[0].external_urls.spotify +
+                "\n*************************\n\r\n\r\n";
+            log(info);
 
         })
         .catch(function (err) {
@@ -104,29 +107,47 @@ function spotifyThisSong() {
         });
 
 }
-//movie-this
-//Title of movie, year it came out, IMBD rating, rottenToms rating, Country produced, language, plot, and actors
-//Defaults to Mr. Nod
-// movieThis(){
-//     axios.get()
-// }
+
+function movieThis() {
+    var movie = mediaSearch;
+    if (!mediaSearch || mediaSearch == undefined) {
+        movie = "Mr. Nobody"
+    }
+    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + movie).then(
+        function (response) {
+            let title = response.data.Title;
+            let year = response.data.Year;
+            let IMBDrating = response.data.Ratings[0].Value;
+            let rottenToms = response.data.Ratings[1].Value;
+            let country = response.data.Country;
+            let language = response.data.Language;
+            let plot = response.data.Plot;
+            let info = "\n\r\n\r\n*************************" +
+                "\nTitle: " + title +
+                "\nYear Released: " + year +
+                "\nIMD Rating: " + IMBDrating +
+                "\nRotten Tomatoes Rating:  " + rottenToms +
+                "\nCountry Produced In: " + country +
+                "\nLanguage(s): " + language +
+                "\nPlot: " + plot +
+                "\nActors: " + response.data.Actors +
+                "\n*************************\n\r\n\r\n";
+            log(info);
+        }
+    )
+};
+
 
 //do-what-it-says
-// function doWhatItSays() {
-//     fs.read("random.text", "utf8", function (err, data) {
-//         if (err) {
-//             return console.log(err);
-//         }
-//         spotifyThisSong()
-//     }
-
-
-
-// In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-        //append
-
-    //     fs.appendFile("log.txt", results, function (err) {
-    //     if (err) {
-    //         return console.log(err);
-    //     }
-    // });
+function doWhatItSays() {
+    fs.readFile("random.text", "utf8", function(error, data) {
+        if(error){
+            return console.log(error);
+        }
+        var input = data.split(",");
+        action = input[0];
+        mediaSearch = input[1];
+        evaluateAction();
+        
+    })
+}
